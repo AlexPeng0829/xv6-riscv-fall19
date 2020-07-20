346f0a60f561b8018a81d75859d9658d1cd48e02
Alex Peng <pengleihit@163.com>
2020-07-20 09:48:50 +0800

    finish first part of lab lock

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,15 +18,29 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct kmem {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  int page_num;
+};
+
+struct kmem_collection{
+  struct kmem item[NCPU];
+  struct spinlock lock;
+  int cyclic_iter;
+} kmems;
+
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  initlock(&kmems.lock, "kmems");
+  kmems.cyclic_iter = 0;
+  for(int i = 0; i < NCPU; ++i)
+  {
+    initlock(&kmems.item[i].lock, "kmem");
+    kmems.item[i].page_num = 0;
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -36,7 +50,57 @@ freerange(void *pa_start, void *pa_end)
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  {
     kfree(p);
+  }
+}
+
+int
+borrow_mem(int cpu_id)
+{
+  int page_borrowed = 0;
+  acquire(&kmems.lock);
+  for(int loop_count = 0; loop_count < NCPU; ++loop_count)
+  {
+    // Skip current cpu's kmem to avoid deadlock
+    if(kmems.cyclic_iter == cpu_id)
+    {
+      kmems.cyclic_iter = (kmems.cyclic_iter + 1) % NCPU;
+      continue;
+    }
+
+    acquire(&kmems.item[kmems.cyclic_iter].lock);
+    if(kmems.item[kmems.cyclic_iter].page_num > 1)
+    {
+      acquire(&kmems.item[cpu_id].lock);
+      page_borrowed = kmems.item[kmems.cyclic_iter].page_num/2;
+      for(int i = 0; i < page_borrowed; ++i)
+      {
+        struct run* r = kmems.item[kmems.cyclic_iter].freelist;
+        kmems.item[kmems.cyclic_iter].freelist = r->next;
+        kmems.item[kmems.cyclic_iter].page_num--;
+
+        r->next = kmems.item[cpu_id].freelist;
+        kmems.item[cpu_id].freelist = r;
+        kmems.item[cpu_id].page_num++;
+      }
+      release(&kmems.item[cpu_id].lock);
+      release(&kmems.item[kmems.cyclic_iter].lock);
+
+      kmems.cyclic_iter = (kmems.cyclic_iter + 1) % NCPU;
+      release(&kmems.lock);
+      return page_borrowed;
+    }
+    else
+    {
+      release(&kmems.item[kmems.cyclic_iter].lock);
+      kmems.cyclic_iter = (kmems.cyclic_iter + 1) % NCPU;
+    }
+
+  }
+  release(&kmems.lock);
+  return page_borrowed;
+
 }
 
 // Free the page of physical memory pointed at by v,
@@ -47,6 +111,11 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  int cpu_id;
+
+  push_off();
+  cpu_id = cpuid();
+  pop_off();
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -56,10 +125,11 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmems.item[cpu_id].lock);
+  r->next = kmems.item[cpu_id].freelist;
+  kmems.item[cpu_id].freelist = r;
+  kmems.item[cpu_id].page_num++;
+  release(&kmems.item[cpu_id].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,13 +139,32 @@ void *
 kalloc(void)
 {
   struct run *r;
+  int cpu_id;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  cpu_id = cpuid();
+  pop_off();
+
+  acquire(&kmems.item[cpu_id].lock);
+  r = kmems.item[cpu_id].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
-
+  {
+    kmems.item[cpu_id].freelist = r->next;
+    kmems.item[cpu_id].page_num--;
+    release(&kmems.item[cpu_id].lock);
+  }
+  else
+  {
+    release(&kmems.item[cpu_id].lock);
+    if(borrow_mem(cpu_id))
+    {
+      acquire(&kmems.item[cpu_id].lock);
+      r = kmems.item[cpu_id].freelist;
+      kmems.item[cpu_id].freelist = r->next;
+      kmems.item[cpu_id].page_num--;
+      release(&kmems.item[cpu_id].lock);
+    }
+  }
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
